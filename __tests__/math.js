97ded7024ec3be3c914dadc7a103acdfa1405ed1
Amadou <atidiane.camara@gmail.com>
2022-07-05 11:37:37 +0200

    remove frozen option to pipe job config

--- __tests__/math.js
+++ __tests__/math.js
@@ -5,4 +5,12 @@ describe('Math test', () => {
         const a = 2 + 2;
         expect(a).toEqual(4);
     });
+    it('should substract num', () => {
+        const a = 2 - 2;
+        expect(a).toBe(0);
+    });
+    it('should fail substract num', () => {
+        const a = 2 - 2;
+        expect(a === 4).toBeFalsy();
+    });
 });
