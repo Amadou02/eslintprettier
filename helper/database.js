048fda88910ee0dcd819be450805f5af2aac4b2f
Amadou <atidiane.camara@gmail.com>
2022-07-09 13:05:30 +0200

    add fake data for seed users

--- helper/database.js
+++ helper/database.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
+
 const getDbInstance = async () => {
     try {
-        const db = await mongoose.connect(process.env.DB_URI, {
+        const db = await mongoose.connect(process.env.DB_DEV_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log(`L'application est connectée à MongoDB Atlas`);
+        // console.log(`L'application est connectée à MongoDB Atlas`);
         return db;
     } catch (error) {
-        console.log(error.message);
+        // console.log(error.message);
+        return false;
     }
 };
 
